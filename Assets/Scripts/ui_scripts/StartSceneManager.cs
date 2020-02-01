7996c224e6253aee7205a591eb3e1709d4d3d102
Mehmet NAMIDURU <mehmetnamiduru1@d34f.wls.metu.edu.tr>
2020-02-01 23:54:01 +0300

    ggj-2020 :: setting play button actions

---
+++ Assets/Scripts/ui_scripts/StartSceneManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StartSceneManager : MonoBehaviour
+{
+
+    private bool isUpward = true;
+    private bool isAnimationStarted = false;
+
+    private float destinationPoint = 120;
+
+    void Update() {
+        if (isAnimationStarted) {
+            if (isUpward) {
+                transform.position = new Vector2(transform.position.x, transform.position.y + 1);
+                if (transform.position.y > 120) {
+                    isUpward = false;
+                }
+            } else {
+                transform.position = new Vector2(transform.position.x, transform.position.y - 1);
+                if (transform.position.y < 60) {
+                    isUpward = true;
+                }
+            }
+        }
+    }
+
+    public void LoadNextScene() {
+        this.disableButtonInteraction();
+        StartCoroutine(animatePlayButton());
+    }
+
+    private IEnumerator animatePlayButton() {
+        toggleAnimationStarted();
+        
+        yield return new WaitForSeconds(2.1f);
+        
+        toggleAnimationStarted();
+        
+        SceneManager.LoadScene("MuratScene");
+    }
+
+    private void toggleAnimationStarted() {
+        if (this.isAnimationStarted) {
+            isAnimationStarted = false;
+        } else {
+            isAnimationStarted = true;
+        }
+    }
+
+    private void disableButtonInteraction() {
+        this.GetComponent<Button>().interactable = false;
+    }
+}
